e9f38f9cabc2e3ef54cd21a16bd572df90decd73
Rafael Oliveira <faeltwister@gmail.com>
2022-02-22 10:19:39 -0300

    seleção simples no pascal
    # Programa simples onde mostra apenas uma instrução na tela usando if & else.

---
+++ selecaosimples.pas
@@ -0,0 +1,17 @@
+Program AlgoritmoDeSelecaoSimples ;
+		var x,y : string;
+	
+						 {Exemplo com apenas 1 instrução na tela}
+Begin
+        Writeln('Bom dia! é hora de acordar.');      
+        Writeln('...........................');
+        Writeln('O café da Manhã está pronto!');
+        Writeln('...........................');
+        Writeln('Você vai à praia ? (s) ou (n)');
+        readln (x);
+        if ((x='s') or (x='S')) then
+        	writeln ('Ahh... que pena que não sou humano para curtir a praia =(' )
+				else
+					Writeln ('Uhull...vem x1 no xadrez então! ')  ;
+
+End.
